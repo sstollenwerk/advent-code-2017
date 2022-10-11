10b05c4f7e3af63f0bc26079327833b624de0c10
sstollenwerk <sstollenwerk@yahoo.co.nz>
2022-10-11 15:05:27 +1300

    do day06 part2

--- src/day06.py
+++ src/day06.py
@@ -1,8 +1,10 @@
 from collections.abc import Iterator, Callable, Iterable
+import enum
 from typing import TypeVar
 from itertools import islice, tee
+from operator import eq
 
-from generic import get_file
+from generic import get_file, uncurry
 
 
 T = TypeVar("T")
@@ -30,7 +32,13 @@ def iterate(k: T, f: Callable[[T], T]) -> Iterator[T]:
 
 
 def loop_len(xs: Iterator[T]) -> int:
-    pass
+    a,b_ = tee(xs, 2)
+    b = islice(b_, 0, None, 2)
+    pairs = zip(a,b)
+    posses = (i for i,el in enumerate(pairs) if uncurry(eq)(el) )
+    next(posses)
+    x,y = next(posses), next(posses)
+    return y-x
 
 
 def part1(s: str) -> int:
@@ -43,9 +51,28 @@ def part1(s: str) -> int:
         data.add(el)
     assert False
 
+def part2_functional(s: str) -> int:
+    banks = tuple(map(int, s.strip().split()))
+    return loop_len(iterate(banks, step))
+
+def part2_imperative(s: str) -> int:
+
+    banks = tuple(map(int, s.strip().split()))
+    data = dict()
+    for i, el in enumerate(iterate(banks, step)):
+        # print(el)
+        if el in data.keys():
+            return i - data[el]
+        data[el] = i
+    assert False
 
 def part2(s: str) -> int:
-    pass
+    a = part2_functional(s)
+    b = part2_imperative(s)
+    assert a == b
+    return a
+
+    
 
 
 def main():
