b4ff9d3741286c7491571fb0dcd44a5ea27cb426
sstollenwerk <sstollenwerk@yahoo.co.nz>
2022-10-12 18:57:35 +1300

    Do day10 part2

--- src/day10.py
+++ src/day10.py
@@ -1,4 +1,6 @@
 from collections import deque
+from functools import reduce
+from operator import xor
 
 from generic import get_file
 
@@ -17,13 +19,14 @@ def parse(s: str) -> list[int]:
     return list(map(int, s.split(",")))
 
 
+def parse2(s: str) -> list[int]:
+    return list(map(ord, s))
+
+
 def knot(xs: list[int], lengths: list[int]) -> list[int]:
     i = 0
     for skip, L in enumerate(lengths):
         xs = reverse(xs, i, L)
-        ## print(i, skip, L)
-        ## print(xs)
-        ## print()
         i += L + skip
     return xs
 
@@ -36,8 +39,28 @@ def part1(s: str) -> int:
     return r[0] * r[1]
 
 
-def part2(s: str) -> int:
-    pass
+def chunks(xs, n):
+    if not xs:
+        return []
+    return [xs[:n]] + chunks(xs[n:], n)
+
+
+def sing_block(xs: list[int]) -> str:
+    assert len(xs) > 0
+    val = reduce(xor, xs)
+    return "{:0>2x}".format(val)
+
+
+def to_dense(xs: list[int]) -> str:
+    parts = chunks(xs, 16)
+    return "".join(map(sing_block, parts))
+
+
+def part2(s: str) -> str:
+    xs = list(range(256))
+    lengths = (parse2(s) + [17, 31, 73, 47, 23]) * 64
+    sparse = knot(xs, lengths)
+    return to_dense(sparse)
 
 
 def main():
