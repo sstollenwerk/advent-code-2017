74bf6e9acde7d12603c7fc8561d3f07df3904aca
sstollenwerk <sstollenwerk@yahoo.co.nz>
2022-10-14 15:06:20 +1300

    Do day12 part1

---
+++ src/day12.py
@@ -0,0 +1,47 @@
+from generic import get_file
+
+
+Node = str
+Connections = dict[Node, set[Node]]
+
+
+def parse(s: str) -> Connections:
+    res = {}
+    for r in s.split("\n"):
+        k, v_ = r.split(" <-> ")
+        v = set(i.strip() for i in v_.split(","))
+        res[k] = v
+    return res
+
+
+def connects(conn: Connections, n: Node) -> set[Node]:
+    res = set(n)
+
+    currents = set(n)
+
+    while currents:
+        part = set.union(*(conn[c] for c in currents))
+        currents = part - res
+        res |= part
+    return res
+
+
+def part1(s: str) -> int:
+    cons = parse(s)
+    res = connects(cons, "0")
+    print(res)
+    return len(res)
+
+
+def part2(s: str) -> int:
+    pass
+
+
+def main():
+    s = get_file(12).strip()
+    print(f"{part1(s) = }")
+    print(f"{part2(s) = }")
+
+
+if __name__ == "__main__":
+    main()
