f15624791bfccc723fb111905a78b86fb2fe4828
sstollenwerk <sstollenwerk@yahoo.co.nz>
2022-10-15 19:06:53 +1300

    Do day15 part2

--- src/day15.py
+++ src/day15.py
@@ -10,24 +10,31 @@ def parse(s: str):
     return [int(r.split()[-1]) for r in rows]
 
 
-def make_gen(start: int, factor: int):
+def make_gen(start: int, factor: int, multiple: int):
     n = start
     while True:
         n *= factor
         n %= 2147483647
-        yield n % (2**16)
+        if not n % multiple:
+            yield n % (2**16)
 
 
 def part1(s: str) -> int:
     factors = [16807, 48271]
-    gets = map(make_gen, parse(s), factors)
+    multiples = [1, 1]
+    gets = map(make_gen, parse(s), factors, multiples)
     sequences = [islice(i, 40_000_000) for i in gets]
 
     return ilen(filter(uncurry(eq), zip(*sequences)))
 
 
 def part2(s: str) -> int:
-    pass
+    factors = [16807, 48271]
+    multiples = [4, 8]
+    gets = map(make_gen, parse(s), factors, multiples)
+    sequences = [islice(i, 5_000_000) for i in gets]
+
+    return ilen(filter(uncurry(eq), zip(*sequences)))
 
 
 def main():
