f978305cfbd15358c4175d1ea643b74840fc4b0a
sstollenwerk <sstollenwerk@yahoo.co.nz>
2022-10-16 12:09:30 +1300

    Do day18 part2 That was a pain

--- src/day18.py
+++ src/day18.py
@@ -1,5 +1,7 @@
-from collections import defaultdict
-from dataclasses import dataclass, replace, asdict
+from collections import defaultdict, deque
+from dataclasses import dataclass, asdict
+from copy import deepcopy
+
 
 from generic import get_file, lines, map_values
 
@@ -64,7 +66,7 @@ def parse_row(s: str) -> Inst:
     return class_(*xs)
 
 
-def interpret(registers: defaultdict[str, int], commands: list[Inst]) -> int:
+def interpret_part1(registers: defaultdict[str, int], commands: list[Inst]) -> int:
     seen = None
 
     def get_val(s: str | int) -> int:
@@ -115,11 +117,85 @@ def parse(s: str) -> list[Inst]:
 def part1(s: str) -> int:
     instructions = parse(s)
     registers = defaultdict(int)
-    return interpret(registers, instructions)
+    return interpret_part1(registers, instructions)
+
+
+def interpret_part2(
+    registers: defaultdict[str, int], commands: list[Inst], vals: deque[int]
+):
+    def get_val(s: str | int) -> int:
+        if isinstance(s, int):
+            return s
+        return registers[s]
+
+    yield
+    i = 0
+    while 0 <= i < len(commands):
+        c = commands[i]
+        d = type(c)(**map_values(get_val, asdict(c)))
+        i += 1
+        ##match type(d):
+        if isinstance(d, Snd):
+            r = d.x
+            yield r
+        elif isinstance(d, Set):
+            registers[c.x] = d.y
+
+        elif isinstance(d, Add):
+            registers[c.x] += d.y
+
+        elif isinstance(d, Mul):
+            registers[c.x] *= d.y
+
+        elif isinstance(d, Mod):
+            registers[c.x] %= d.y
+
+        elif isinstance(d, Rcv):
+            if not vals:
+                i -= 1
+                yield
+
+            else:
+                registers[c.x] = vals.pop()
+
+        elif isinstance(d, Jgz):
+            if d.x > 0:
+                i += d.y - 1
+
+        else:
+            assert False
 
 
 def part2(s: str) -> int:
-    pass
+    instructions = parse(s)
+    registers = defaultdict(int)
+
+    insts = [[], []]
+
+    datas = [deque([]), deque([])]
+
+    comps = []
+    for i in range(2):
+        r = deepcopy(registers)
+        r["p"] = i
+        inst = deepcopy(instructions)
+        k = i
+        comp = interpret_part2(r, inst, datas[i])
+        next(comp)
+
+        comps.append(comp)
+
+    while True:
+        seen = False
+        for i in range(2):
+            d = next(comps[i])
+            if d is not None:
+                insts[1 - i].append(d)
+                datas[1 - i].appendleft(d)
+                seen = True
+        if not seen:
+            break
+    return len(insts[0])
 
 
 def main():
