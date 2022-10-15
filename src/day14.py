05c36bb5f79d4d89dd6230f3611f1ca753c45574
sstollenwerk <sstollenwerk@yahoo.co.nz>
2022-10-15 16:17:49 +1300

    Do day14 part2 Mutiprocessing for speedup in bits include leading zeros

--- src/day14.py
+++ src/day14.py
@@ -1,9 +1,11 @@
+from multiprocessing import Pool
+
 from day10 import parse2, knot, to_dense
 from generic import get_file, flatten
 
 
 def bits(s: str):
-    data = bin(int(s, base=16))[2:]
+    data = flatten(bin(int(c, base=16))[2:].zfill(4) for c in s)
     return list(map(bool, map(int, data)))
 
 
@@ -12,14 +14,37 @@ def row(p: list[int]):
     return bits(to_dense(knot(xs, p)))
 
 
-def part1(s: str) -> int:
+def make_grid(s: str) -> set[complex]:
     parts = [(parse2(f"{s}-{i}") + [17, 31, 73, 47, 23]) * 64 for i in range(128)]
+    p = Pool(None)
+    b = list(p.imap(row, parts))
 
-    return sum(flatten(map(row, parts)))
+    return {complex(x, y) for y, r in enumerate(b) for x, c in enumerate(r) if c}
+
+
+def part1(s: str) -> int:
+
+    return len(make_grid(s))
+
+
+def adj(p: complex) -> list[complex]:
+    return [p + c for c in [1, -1, 1j, -1j]]
+
+
+def groups(g_: set[complex]) -> list[set[complex]]:
+    res: list[set[complex]] = []
+    g = g_.copy()
+    while g:
+        group = {min(g, key=abs)}
+        while (next_ := g & (set(flatten(map(adj, group))) | group)) != group:
+            group = next_
+        res.append(group)
+        g -= group
+    return res
 
 
 def part2(s: str) -> int:
-    pass
+    return len(groups(make_grid(s)))
 
 
 def main():
